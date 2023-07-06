dc79928258c05fafd12ebbb24f0026c80e612f36
Marco de Jongh <1107647+marcodejongh@users.noreply.github.com>
2023-07-06 11:32:58 +1000

    Fix fzf config now its moved to brew

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/Cellar/fzf/0.42.0/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/Cellar/fzf/0.42.0/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.42.0/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source "/opt/homebrew/Cellar/fzf/0.42.0/shell/key-bindings.bash"
